30678dfa1225d894ad88cad06eb5583735722355
leilei405 <1601178425@qq.com>
2026-04-07 23:47:26 +0800

    feat: Python 嵌套循环学习

--- 04章/09_嵌套循环.py
+++ 04章/09_嵌套循环.py
@@ -1,7 +1,6 @@
-for day in range(31):
-    print(f'📅第{day + 1}天')
-    for group in range(3):
-        print(f"这是第一组{group + 1}仰卧起坐")
-    print(f"✅第{day + 1}任务已完成！明天继续\n")
-
-print("一个月健身完成")
+for day in range(1, 31):
+    print(f'📅第{day}天')
+    for group in range(1, 3):
+        print(f"这是第一组{group}仰卧起坐")
+    print(f"✅第{day}任务已完成！明天继续\n")
+print("一个月健身目标完成")
