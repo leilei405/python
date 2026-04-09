941a6f5d41471bce0b6970ab1add0c684fe3ac50
leilei405 <1601178425@qq.com>
2026-04-10 00:44:06 +0800

    feat: Python 函数递归

--- 05章/06_函数递归调用.py
+++ 05章/06_函数递归调用.py
@@ -3,4 +3,18 @@ def test(n):
     if n > 1:
         test(n - 1)
 
-test(5)
+# test(5)
+
+def test2(n):
+    """
+    求阶乘
+    :param n: n的阶乘
+    :return: 最终计算的值
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * test2(n - 1)
+
+res = test2(10)
+print(res)
