6bd561cfe5c7beec4a7a3a6624aa7215e1085837
leilei405 <1601178425@qq.com>
2026-04-10 01:15:09 +0800

    feat: Python 综合案例

--- 05章/07_综合案例.py
+++ 05章/07_综合案例.py
@@ -1,9 +1,12 @@
 total = 0
 svg = 0
+
+
 def success():
     print(f"总数：{total}, 平均值{svg}")
     if total > 100:
         print("✅恭喜！挑战成功")
+
 
 def main():
     print("【仰卧起坐】【7天】👊🏻挑战赛（请输入每天的数量）")
@@ -18,5 +21,6 @@ def main():
     svg = float(total / days)
     print("【仰卧起坐】【7天】健身总结")
     success()
+
 
 main()
