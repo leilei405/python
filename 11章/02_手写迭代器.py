0edcc4e35fa268692aa59192d553f8bc859540f1
leilei405 <1601178425@qq.com>
2026-05-04 23:32:37 +0800

    feat: 今天不想代码

--- 11章/02_手写迭代器.py
+++ 11章/02_手写迭代器.py
@@ -171,5 +171,5 @@ for num in my_iterable:
     print(num, end=" ")  # 1 2 3
 print()
 
-for num in my_iterable:  # 可以多次遍历 是的
+for num in my_iterable:  # 可以多次遍历 是的  今天不想写代码
     print(num, end=" ")  # 1 2 3
